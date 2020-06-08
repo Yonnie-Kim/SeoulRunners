80ad4f706f0e2aea5257f28cadbcb7dae7107e73
Yonnie-Kim <53686910+Yonnie-Kim@users.noreply.github.com>
2020-06-08 11:22:38 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient           # pymongo 임포트
+from flask import Flask, render_template
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta                      # dbsparta 생성
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
