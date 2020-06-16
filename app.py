c2cbd8c1a72135ace8ffed60297bd5bf39e5bbf3
Yonnie-Kim <53686910+Yonnie-Kim@users.noreply.github.com>
2020-06-16 23:45:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient           # pymongo 임포트
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
@@ -14,6 +16,16 @@ def home():
 @app.route('/mapprac')
 def mapprac():
     return render_template('mapprac.html')
+
+
+@app.route('/mapprac2')
+def mapprac2():
+    return render_template('mapprac2.html')
+
+
+@app.route('/tmapprac')
+def tmapprac():
+    return render_template('tmapprac.html')
 
 
 if __name__ == '__main__':
