722e1eb927257e3cd2504e204c62467598b0e670
Yonnie-Kim <53686910+Yonnie-Kim@users.noreply.github.com>
2020-06-28 17:48:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@52.78.25.223', 27017)
 db = client.dbsparta                      # dbsparta 생성
 
 
@@ -27,19 +27,6 @@ def mapprac2():
 def tmapprac():
     return render_template('tmapprac.html')
 
-# 경로입력
-# route = {
-#     "routeNo": 2,
-#     "originX": 126.976462,
-#     "originY": 37.575697,
-#     "destinationX": 126.993741,
-#     "destinationY": 37.570536,
-#     "passX": 126.985383,
-#     "passY": 37.576479,
-#     "originName": "경복궁",
-#     "destinationName": "종로3가",
-# }
-# db.runningroutes.insert_one(route)
 
 # DB정보를 불러오는 json db 페이지
 
